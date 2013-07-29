0c56d08ed629c7d5648df3b373c669998a72d0a5
Bart Veneman <bartveneman@gmail.com>
2013-07-29 11:34:56 +0200

    Changes to make jslint happy, removed unused css function

--- fittext.js
+++ fittext.js
@@ -1,4 +1,5 @@
-/*!	
+/*jslint browser: true*/
+/*! 
 * FitText.js 1.0 jQuery free version
 *
 * Copyright 2011, Dave Rupert http://daverupert.com 
@@ -8,48 +9,49 @@
 *
 * Date: Tue Aug 09 2011 10:45:54 GMT+0200 (CEST)
 */
-(function(){
-  var css = function (el, prop) {
-    return window.getComputedStyle ? getComputedStyle(el).getPropertyValue(prop) : el.currentStyle[prop];
-  };
-  
-  var addEvent = function (el, type, fn) {
-    if (el.addEventListener)
-      el.addEventListener(type, fn, false);
-		else
-			el.attachEvent('on'+type, fn);
-  };
+(function (w) {
+    "use strict";
 
-  window.fitText = function (el, kompressor) {
+    var addEvent = function (el, type, fn) {
+            if (el.addEventListener) {
+                el.addEventListener(type, fn, false);
+            } else {
+                el.attachEvent('on' + type, fn);
+            }
+        };
 
-    var settings = {
-      'minFontSize' : -1/0,
-      'maxFontSize' : 1/0
+    w.fitText = function (el, kompressor) {
+        var settings = {
+                'minFontSize' : -1 / 0,
+                'maxFontSize' : 1 / 0
+            },
+
+            fit = function (el) {
+                var compressor = kompressor || 1,
+
+                    resizer = function () {
+                        el.style.fontSize = Math.max(Math.min(el.clientWidth / (compressor * 10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)) + 'px';
+                    };
+
+                // Call once to set.
+                resizer();
+
+                // Bind events
+                // If you have any js library which support Events, replace this part
+                // and remove addEvent function (or use original jQuery version)
+                addEvent(window, 'resize', resizer);
+            },
+            i;
+
+        if (el.length) {
+            for (i = 0; i < el.length; i += 1) {
+                fit(el[i]);
+            }
+        } else {
+            fit(el);
+        }
+
+        // return set of elements
+        return el;
     };
-
-    var fit = function (el) {
-      var compressor = kompressor || 1;
-
-      var resizer = function () {
-        el.style.fontSize = Math.max(Math.min(el.clientWidth / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)) + 'px';
-      };
-
-      // Call once to set.
-      resizer();
-
-      // Bind events
-      // If you have any js library which support Events, replace this part
-      // and remove addEvent function (or use original jQuery version)
-      addEvent(window, 'resize', resizer);
-    };
-
-    if (el.length)
-      for(var i=0; i<el.length; i++)
-        fit(el[i]);
-    else
-      fit(el);
-
-    // return set of elements
-    return el;
-  };
-})();
+}(window));
